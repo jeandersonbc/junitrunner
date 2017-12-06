22ae59cabcdec20e0efccff1eb6aeedfa0b865a0
Jeanderson Candido <jeandersonbc@gmail.com>
2017-12-06 11:44:19 -0300

    Updated Experiment code

--- src/main/java/br/ufpe/cin/spg/testrunner/experiment/FlakySubSet.java
+++ src/main/java/br/ufpe/cin/spg/testrunner/experiment/FlakySubSet.java
@@ -1,15 +1,18 @@
 package br.ufpe.cin.spg.testrunner.experiment;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.runner.Computer;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 import br.ufpe.cin.spg.testrunner.App;
 import br.ufpe.cin.spg.testrunner.TestListener;
 
-public class FlakinessSubset {
+public class FlakySubSet {
 
 	public static void main(String[] args) throws ClassNotFoundException {
 		int N = Integer.parseInt(args[0]);
@@ -26,8 +29,20 @@ public class FlakinessSubset {
 			results[i] = result;
 		}
 
+		System.out.println("--- EXPERIMENT SUMMARY ---");
 		for (Result r : results) {
 			App.displayReport(r);
+		}
+		Map<String, Integer> freq = new HashMap<>();
+		for (Result r : results) {
+			for (Failure f : r.getFailures()) {
+				Integer value = freq.get(f.getTestHeader());
+				freq.put(f.getTestHeader(), (value == null) ? 1 : value + 1);
+			}
+		}
+		System.out.println(String.format("# of runs: %d", N));
+		for (Map.Entry<String, Integer> entry : freq.entrySet()) {
+			System.out.println(String.format("%s: %d", entry.getKey(), entry.getValue()));
 		}
 	}
 }
